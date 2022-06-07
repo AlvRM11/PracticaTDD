9afe2d7c61f2df76394a055e3a12c4586f5d7998
Alvaro <usr2725@salesianos-lacuensta.net>
2022-06-07 10:53:14 +0100

    Test Fallido 2
    La prueba que cumple el segundo requisito ha fallado

--- test_contador.py
+++ test_contador.py
@@ -10,5 +10,12 @@ class Test_Contador(unittest.TestCase):
         self.assertEqual(contador1.incremento, 2)
         self.assertEqual(contador1.limite, 5)
 
+    def test2(self):
+        contador2 = Contador(limite=3)
+
+        self.assertEqual(contador2.vInicial, 0)
+        self.assertEqual(contador2.incremento, 1)
+        self.assertEqual(contador2.limite, 3)
+        
 if __name__ == '__main__':
     unittest.main()
