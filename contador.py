a874c60c36c3bc30b5f88d4da555cee3fec4824b
Alvaro <usr2725@salesianos-lacuensta.net>
2022-06-07 10:57:10 +0100

    Test Exitoso 2
    Se ha escrito el código mínimo para que pase el test y se cumpla el segundo requisito

--- contador.py
+++ contador.py
@@ -1,6 +1,6 @@
 class Contador():
     
-    def __init__(self, vInicial, incremento, limite):
+    def __init__(self, vInicial=0, incremento=1, limite=3):
 
         self.vInicial = vInicial
         self.incremento = incremento
