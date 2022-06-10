7fbbf738a4460527c81baee9542f191f6db64d73
Alvaro <usr2725@salesianos-lacuensta.net>
2022-06-10 01:05:23 +0100

    Test Exitoso 3
    Se ha escrito el código necesario para que funcione el test3 y así cumplir el cuarto requisito

--- contador.py
+++ contador.py
@@ -14,3 +14,20 @@ class Contador():
 
     def get_limite(self):
         return self.__limite
+
+    def incrementador(self):
+        vInicial = self.get_vInicial()
+        incremento = self.get_incremento()
+        limite = self.get_limite()
+        contador = vInicial
+
+        bucle = True
+
+        while bucle:
+            if (contador < limite):
+                print(f'valor actual: {contador}')
+                contador += incremento
+            else:
+                print('Se superó el límite')
+                bucle = False
+                return contador
